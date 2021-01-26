fccd7e1321241a75cc08deebcb6dcf11047347d2
Sandro Santos <sandro.dev@gmail.com>
2021-01-26 19:46:39 -0300

    refactor: lint the code

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,5 @@
-import Sequelize, { Model } from 'sequelize';
-import bcrypt from 'bcrypt';
+import Sequelize, { Model } from "sequelize";
+import bcrypt from "bcrypt";
 
 class User extends Model {
   static init(sequelize) {
@@ -12,10 +12,10 @@ class User extends Model {
       },
       {
         sequelize,
-      },
+      }
     );
 
-    this.addHook('beforeSave', async user => {
+    this.addHook("beforeSave", async (user) => {
       if (user.password) {
         user.password = await bcrypt.hash(user.password, 8);
       }

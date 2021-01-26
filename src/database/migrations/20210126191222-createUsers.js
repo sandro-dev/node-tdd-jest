0878d86049a3caa252aee70086884ba6d1d8626d
Sandro Santos <sandro.dev@gmail.com>
2021-01-26 19:19:26 -0300

    feat(User): migration, seed and model

---
+++ src/database/migrations/20210126191222-createUsers.js
@@ -0,0 +1,51 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    if (process.env.DB_DIALECT === 'postgres') {
+      queryInterface.sequelize.query(
+        'CREATE EXTENSION IF NOT EXISTS "uuid-ossp"',
+      );
+    }
+
+    queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.UUID(),
+        allowNull: false,
+        primaryKey: true,
+        generationStrategy: 'uuid',
+        defaultValue: Sequelize.literal('uuid_generate_v4()'),
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+        unique: true,
+      },
+
+      password: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+
+      phone: {
+        type: Sequelize.STRING(20),
+        allowNull: true,
+      },
+
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => queryInterface.dropTable('users'),
+};
---
+++ src/database/seeds/20210126191302-insertUsers.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: QueryInterface =>
+    QueryInterface.bulkInsert(
+      'users',
+      [
+        {
+          name: 'Sandro',
+          email: 'sandro.dev@gmail.com',
+          password: bcrypt.hashSync('Sa123456', 8),
+          phone: '+5571988928742',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          name: 'Zeca',
+          email: 'admin@sandrosantos.dev',
+          password: bcrypt.hashSync('Ad123456', 8),
+          phone: '+5571988776655',
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+
+      {},
+    ),
+
+  down: () => {},
+};
